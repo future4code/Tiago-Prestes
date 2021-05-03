d95a52313005c55b48d040d601c58ff2d1f7bad9
TiagoPrestes <tiagoprestes88@gmail.com>
2021-05-03 16:28:58 -0300

    Entrega exercício aula 03 - Variáveis

---
+++ semana1/variaveis/index.js
@@ -0,0 +1,53 @@
+/* 
+RESPOSTAS INTERPRETAÇÃO:
+
+1. console.log(b) imprime 10
+   console.log(a, b) imprime 10 e 5
+
+2. console.log(a, b, c) imprime 10, 10 e 10
+
+3. p = horasTrabalhadas
+   t = remuneracaoDiaria
+*/
+
+/*1.
+1-D. Undefined - O valor das variaveis não foram definidos
+1-F. Impreme string - é o tipo atribuido a elas.*/
+
+/*
+const nome = prompt("Qual seu nome?")
+const idade = prompt("Qual seu idade")
+
+console.log(nome)
+console.log(idade)
+console.log(typeof nome)
+console.log(typeof idade)
+console.log("Olá",nome,", você tem",idade,"anos.")
+*/
+
+/*2.
+
+const usandoRoupa = "SIM"
+const planetaTerra = "NÃO"
+const corDoUrurubuFilhote = "SIM"
+console.log("Está usando roupa hoje?",usandoRoupa)
+console.log("A terra é plana?",planetaTerra)
+console.log("O urubu quando nasce é branco?",corDoUrurubuFilhote)
+*/
+
+/*3.
+let aux1 = 25
+let a = aux1
+let aux2 = 10
+let b = aux2
+
+console.log(a)
+console.log(b) */
+
+//Desafio
+
+let dia = Number(prompt("Qual o dia de hoje?"))
+let ano = Number(prompt("Qual o ano estamos?"))
+
+console.log(dia + ano)
+console.log(dia * ano)
