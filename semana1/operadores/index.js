4396bdd24d3fc3e90f794c12cc969a25842f6360
TiagoPrestes <tiagoprestes88@gmail.com>
2021-05-04 16:27:15 -0300

    Exercícios da tarde semana 1 aula 04

--- semana1/operadores/index.js
+++ semana1/operadores/index.js
@@ -1,53 +1,61 @@
-/* 
-RESPOSTAS INTERPRETAÇÃO:
+/*
+1. Leia o código abaixo. Indique todas as mensagens impressas no console,
+SEM EXECUTAR o programa.
+a. FALSE
+b. FALSE
+c. TRUE
+d. Booleano
 
-1. console.log(b) imprime 10
-   console.log(a, b) imprime 10 e 5
+2. Por falta do "Number" antes do comando "prompt" vão se formar strings. 
+   O resultado será o primeiroNumero (ex: 2) concatenado com o segundoNumero (ex: 2) e não a soma.
+   O resultado no console será 22
 
-2. console.log(a, b, c) imprime 10, 10 e 10
-
-3. p = horasTrabalhadas
-   t = remuneracaoDiaria
+3. Adicionar o Number:
+   let primeiroNumero = Number(prompt("Digite um numero!")) //ex: 2
+   let segundoNumero = Number(prompt("Digite outro numero!")) // ex: 2
+   O console vai mostrar 4
 */
 
-/*1.
-1-D. Undefined - O valor das variaveis não foram definidos
-1-F. Impreme string - é o tipo atribuido a elas.*/
+// Exercício 1 
+/*
+let idadeUser = Number(prompt("Digite sua idade"))
+let idadeBff = Number(prompt("Qual a idade do seu melhor amigo?"))
+
+let idadeMaior = idadeUser >= idadeBff
+let diferencaIdade =  idadeUser - idadeBff
+
+console.log("Sua idade é maior que a do seu melhor amigo?", idadeMaior)
+console.log("Diferença de idade", diferencaIdade)
+*/
+
+//Exercício 2
+/*
+let numberPar = Number(prompt("Digite um número par:"))
+console.log("Resto do número par", numberPar % 2)
+//O padrão é sempre 0, pois números pares divididos por 2 e nunca tem sobra.
+//O padrão com número impar é sempre 1, pois um impar divido por 2 sempre sobra 1.
+*/
+
+//Exercício 3
+/*
+let idadeUser = Number(prompt("Digite sua idade"))
+
+console.log("Idade em meses", idadeUser * 12)
+console.log("Idade em dias", idadeUser * 365)
+console.log("Idade em horas", idadeUser * 8760)
+*/
+
+//Exercício 4
 
 /*
-const nome = prompt("Qual seu nome?")
-const idade = prompt("Qual seu idade")
+let primeiroNumero = Number(prompt("Digite um numero de 10 a 100!"))
+let segundoNumero = Number(prompt("Digite outro numero menor que o primeiro!"))
 
-console.log(nome)
-console.log(idade)
-console.log(typeof nome)
-console.log(typeof idade)
-console.log("Olá",nome,", você tem",idade,"anos.")
+let divisivel1 = primeiroNumero % segundoNumero === 0
+let divisivel2 = segundoNumero % primeiroNumero === 1
+
+console.log("O primeiro numero é maior que segundo?", primeiroNumero > segundoNumero)
+console.log("O primeiro numero é igual ao segundo?", primeiroNumero == segundoNumero)
+console.log("O primeiro numero é divisível pelo segundo?", divisivel1)
+console.log("O segundo numero é divisível pelo primeiro?", divisivel2)
 */
-
-/*2.
-
-const usandoRoupa = "SIM"
-const planetaTerra = "NÃO"
-const corDoUrurubuFilhote = "SIM"
-console.log("Está usando roupa hoje?",usandoRoupa)
-console.log("A terra é plana?",planetaTerra)
-console.log("O urubu quando nasce é branco?",corDoUrurubuFilhote)
-*/
-
-/*3.
-let aux1 = 25
-let a = aux1
-let aux2 = 10
-let b = aux2
-
-console.log(a)
-console.log(b) */
-
-/*Desafio
-
-let dia = Number(prompt("Qual o dia de hoje?"))
-let ano = Number(prompt("Qual o ano estamos?"))
-
-console.log(dia + ano)
-console.log(dia * ano)*/
