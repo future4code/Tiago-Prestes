cd033d11e4c9e4df463a096df678b868162664f4
TiagoPrestes <tiagoprestes88@gmail.com>
2021-05-06 17:03:58 -0300

    Exercícios aula06

---
+++ semana1/funcoes/index06.js
@@ -0,0 +1,85 @@
+//Exercícios de interpretação de código
+
+//1.
+
+//a.10 e 50
+//b. Não acontece nada. Nada aparece no console.
+
+//2.
+
+//a. Ela transforma o texto do user e deixa em minúsculas e ele quer saber se a "cenoura" está lá na frase digitada. 
+//b. True, True, True
+
+
+//Exercícios de escrita de código
+
+//1.
+
+//a. 
+
+// const informacoesUsuario = () => {
+//     console.log("Eu sou Tiago, tenho 32 anos, moro em Blumenau e sou estudante")
+// }
+
+// informacoesUsuario()
+
+//b.
+
+// const quatroParametros = (nome, idade, local, profissao ) => {
+//     console.log(`Eu sou ${nome}, tenho ${idade} anos, moro em ${local} e sou ${profissao}.`)
+// }
+
+// quatroParametros('tiago', '32', 'Blumenau', 'estudante')
+
+//2.
+
+//a.
+
+// const doisNumeros = (num1, num2) => {
+//     const somaParametros = num1 + num2
+//     return somaParametros
+// }
+
+// const somaDosNumeros = doisNumeros(2, 8)
+// console.log(somaDosNumeros)
+
+//b.
+
+// const doisNumeros = (num1, num2) => {
+//     const somaParametros = num1 > num2
+//     return somaParametros
+// }
+
+// const somaDosNumeros = doisNumeros(2, 8)
+// console.log(somaDosNumeros)
+
+//c.
+
+// const doisNumeros = (num1) => {
+//     const parOuImpar = num1 % 2 === 0 
+//     return parOuImpar
+// }
+
+// const numeroRecebido = doisNumeros(4)
+// console.log(numeroRecebido)
+
+//d.
+
+// let textoDoUsuario = "Fritando a cabeça"
+
+// const outraFuncao = (texto) => {
+//     return texto.toUpperCase()
+// }
+
+// const resposta = outraFuncao(textoDoUsuario)
+// console.log(resposta)
+// console.log(textoDoUsuario.length)
+
+//Acabou o tempo!!!!!!! AAAAAAAAAAAAAAAAA
+
+
+
+
+
+
+
