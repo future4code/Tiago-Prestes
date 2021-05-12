2a16601ec7a35dfaf763b258e40240274cac0faa
TiagoPrestes <tiagoprestes88@gmail.com>
2021-05-12 16:54:13 -0300

    Exercícios aula09

---
+++ semana2/loops/indexloop.js
@@ -0,0 +1,75 @@
+
+//EXERCÍCIOS DE INTERPRETAÇÃO
+
+//1.
+
+// O valor vai se somar ao "i" e só vai parar quando o "i" retornar mair que 5.
+// Fiz no papel para entender melhor e ficou assim:
+// i = 0
+// i + V(0) = 0
+// i++ = 1
+// i(1) + V(0) = 1
+// i++ = 2
+// i(2) = V(1) = 3
+// i++ = 3
+// i(3) + V(3) = 6
+// i++ = 4
+// i(4) + v(6) = 10
+
+//2.
+
+//a. Vai ser impresso o próximo número maior de 18 (19)
+
+//b. if (numero >= 10 ) poderia usar assim. Sendo assim a letra "a" está errada, imprime do 19 em diante.
+
+//3.
+// ----PULEI
+
+//EXERCÍCIOS DE ESCRITA
+
+//1.
+
+// let quantidadeBichos = Number(prompt("Quantos animais de estimação você tem?"))
+// let bichos = []
+//     if (quantidadeBichos === 0){
+//         console.log("Que pena! Você pode adotar!")
+//     } else if (quantidadeBichos > 0) {
+//         for (let i = 0; i < quantidadeBichos; i++){
+//             bichos[i] = prompt(`Nome do pet ${i+1}`)
+//         }
+//     }
+    
+// console.log(bichos) 
+
+//2.
+
+// const arrayOriginal = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+
+//a.
+
+// const imprimeOriginal = () => {
+//     for (let numero of arrayOriginal){
+//         console.log(numero)
+//     }
+// }
+
+// imprimeOriginal()
+
+//b.
+
+// const imprimeDivididos = () => {
+//     for (let numero of arrayOriginal)
+//         console.log(numero / 10)
+// }
+
+// imprimeDivididos()
+
+//c.
+
+// const imprimePares = () => {
+//     const novoArray = []
+//     for (){
+//         if(arrayOriginal[i] % 2 === 0)
+
+// imprimePares()
+
