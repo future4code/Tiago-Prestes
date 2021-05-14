745fa36b1aaed6cdfbb037ca1c4ca324b9f796fd
TiagoPrestes <tiagoprestes88@gmail.com>
2021-05-14 16:22:54 -0300

    Projeto semana 2

---
+++ semana2/blackjack/nossoJogo.js
@@ -0,0 +1,44 @@
+/**
+ * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+ * 
+ * 
+    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+    
+    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+ * 
+ * 
+ * 
+ */
+
+
+   //  console.log("Bem vindo ao jogo de Blackjack")
+
+
+   //  const comecarJogo = confirm("Deseja começar o jogo?")
+ 
+   //  if (comecarJogo) {
+   //     const carta1Player = comprarCarta()
+   //     const carta2Player = comprarCarta()
+   //     const carta1Cpu = comprarCarta()
+   //     const carta2Cpu = comprarCarta()
+ 
+   //     const playerPontos = carta1Player.valor + carta2Player.valor
+   //     const cpuPontos = carta1Cpu.valor + carta2Cpu.valor
+       
+   //     console.log(`Usuário - cartas: ${carta1Player.texto} ${carta2Player.texto} - pontuação: ${playerPontos}`)
+   //     console.log(`Computador - cartas: ${carta1Cpu.texto} ${carta2Cpu.texto} - pontuação: ${cpuPontos} `)
+ 
+   //     if (playerPontos > cpuPontos) {
+   //        console.log('Usuário ganhou!!!')
+   //     } else if (playerPontos < cpuPontos) {
+   //        console.log('Computador ganhou!!!')
+   //     } else {
+   //        console.log('Empate!')
+   //     }
+ 
+   //  } else {
+   //     console.log('Não teve jogo.')
+   //  } 
+ 
+    
