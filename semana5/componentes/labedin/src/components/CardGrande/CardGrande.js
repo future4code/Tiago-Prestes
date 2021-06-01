d0f133ab8e084aed28e2365a3983ed3c23aade8c
TIAGO PRESTES <83032703+Tiago-prestes@users.noreply.github.com>
2021-06-01 18:53:10 -0300

    Revisão Labedin

--- semana5/componentes/labedin/src/components/CardGrande/CardGrande.js
+++ semana5/componentes/labedin/src/components/CardGrande/CardGrande.js
@@ -1,15 +1,40 @@
 import React from 'react';
-import { BigcardContainer, Imagem, ContainerTexto} from './Style'
+import styled from "styled-components"
+
+const BigcardContainer = styled.div `
+display: flex;
+align-items: center;
+border: 1px solid black;
+padding: 20px 10px;
+margin-bottom: 10px;
+height: 200px;
+`
+
+const Imagem = styled.img `
+width: 70px;
+margin-right: 10px;
+border-radius: 50%;
+`
+
+const Nome = styled.h4 `
+margin-bottom: 15px;
+`
+
+const ContainerTexto = styled.div `
+display: flex;
+flex-direction: column;
+justify-items: flex-start;
+`
 
 function CardGrande(props) {
     return (
         <BigcardContainer>
             <Imagem src={ props.imagem } />
             <ContainerTexto>
-                <Nome>{ props.nome }<Nome />
+                <Nome>{ props.nome }</Nome>
                 <p>{ props.descricao }</p>
-            <ContainerTexto />
-        <BigcardContainer />
+            </ContainerTexto>
+        </BigcardContainer>
     )
 }
 
--- semana5/componentes/labedin/src/components/CardPequeno/CardPequeno.js
+++ semana5/componentes/labedin/src/components/CardPequeno/CardPequeno.js
@@ -1,16 +1,42 @@
 import React from 'react';
-import './CardPequeno.css'
+import styled from "styled-components"
+
+const SmallcardContainer = styled.div `
+    display: flex;
+    align-items: center;
+    border: 1px solid black;
+    padding: 5px 5px;
+    margin-bottom: 10px;
+    width: 550px;
+`
+
+const Imagem = styled.img `
+    width: 25px;
+    margin-right: 10px;
+    border-radius: 50%;
+`
+
+const Nome = styled.h4 `
+    margin-right: 5px;
+    text-align: center;
+`
+
+const ContainerTexto = styled.div `
+    display: flex;
+    justify-items: center;
+`
 
 function CardPequeno(props) {
     return (
-        <div className="smallcard-container">
-            <img src={ props.imagem} />
-            <div>
-                <h4>{ props.titulo}</h4>
+        <SmallcardContainer>
+            <Imagem src={ props.imagem} />
+            <ContainerTexto>
+                <Nome>{ props.titulo}</Nome>
                 <p>{ props.descricao}</p>
-            </div>
-        </div>
+            </ContainerTexto>
+        </SmallcardContainer>
     )
 }
 
 export default CardPequeno;
+
