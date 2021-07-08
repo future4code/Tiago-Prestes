20235c9518224a4cf8ce64c8b5afe11293b733b9
TIAGO PRESTES <83032703+Tiago-prestes@users.noreply.github.com>
2021-07-08 16:52:13 -0300

    Add login

--- semana10/labex-site/src/pages/LoginPage/LoginPage.js
+++ semana10/labex-site/src/pages/LoginPage/LoginPage.js
@@ -1,24 +1,61 @@
-import React from "react"
+import axios from "axios"
+import React, { useState } from "react"
 import { useHistory } from "react-router"
 
 export const LoginPage = () => {
     const history = useHistory()
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
 
     const goBackHome = () => {
         history.goBack()
     }
 
-    const enterLogin = () => {
-        history.push()
+    const onChangeEmail = (event) => {
+        setEmail(event.target.value)
     }
+
+    const onChangePassword = (event) =>{
+        setPassword(event.target.value)
+    }
+
+    const onSubmitLogin = () => {
+        console.log(email, password)
+        const body = {
+            email: email,
+            password: password
+        }
+        axios
+        .post('https://us-central1-labenu-apis.cloudfunctions.net/labeX/tiago-prestes-molina/login', body)
+        .then((res) => {
+            console.log(res)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    }
+
+   
+
+    
 
     return (
         <div>
             <p>Login</p>
-            <input placeholder="E-mail" />
-            <input placeholder="Senha" />
+            <input 
+            placeholder="E-mail"
+            type="email"
+            value={email}
+            onChange={onChangeEmail} 
+            />
+            <input 
+            placeholder="Senha"
+            type="password"
+            value={password}
+            onChange={onChangePassword} 
+            />
             <button onClick={goBackHome} >Voltar</button>
-            <button onClick={goBackHome} >Entrar</button>
+            <button onClick={onSubmitLogin} >Enviar</button>
         </div>
     )
 }
