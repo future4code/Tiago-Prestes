6b6db20d568456c5518b9044b74969cb4f2704d0
TIAGO PRESTES <83032703+Tiago-prestes@users.noreply.github.com>
2021-08-10 19:29:30 -0300

    02

--- semana15/introTS/src/ex1/index.ts
+++ semana15/introTS/src/ex1/index.ts
@@ -8,3 +8,35 @@
 // const meuNumero: string | Number = 0
 
 //c
+// enum cores {
+//     VERMELHO = 'Vermelho',
+//     AZUL = 'Azul',
+//     VERDE = 'Verde',
+//     AMARELO = 'Amarelo'
+// }
+
+// type pessoa = {
+//     nome: string,
+//     idade: Number,
+//     corFavorita: cores
+// } 
+
+// const usuario: pessoa = {
+//     nome: "Joelma",
+//     idade: 80,
+//     corFavorita: cores.VERMELHO
+// }
+
+// const usuario1: pessoa = {
+//     nome: "Jurandir",
+//     idade: 8,
+//     corFavorita: cores.AMARELO
+// }
+
+// const usuario2: pessoa = {
+//     nome: "Jaime",
+//     idade: 40,
+//     corFavorita: cores.AZUL
+// }
+
+
